649791ea4eb8fb5a8d40be41fb33058d7fb900ee
ulysses <ulises316@live.com.mx>
2017-12-06 00:32:40 -0600

    se consume informacion de la base de datos de firebase en el about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { InformacionService } from '../../services/informacion.service'
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
+  constructor(public _is:InformacionService){}
 
-  ngOnInit() {
-  }
+
+
 
 }
